2b2fca199452bfce864ff73a8fcf494faee20650
bamfranceschi <anna.franceschi@gmail.com>
2019-12-19 17:11:30 -0600

    Nav and Routes created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Header from "./components/header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 import "./App.css";
+import About from "./pages/About";
 
 function App() {
   return (
-    <main>
-      <Header />
-    </main>
+    <Router>
+      <div>
+        <Header />
+        <Nav />
+      </div>
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/about" component={About} />
+        <Route path="/contact" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h2>About Me</h2>
+      <p>
+        Spicy jalapeno bacon ipsum dolor amet in labore filet mignon, sed rump
+        ut chicken do fatback qui ullamco veniam. Frankfurter jerky ball tip,
+        enim magna in beef ribs shank spare ribs ribeye burgdoggen venison
+        cupidatat beef pork belly. Frankfurter ham hock veniam eiusmod.
+        Adipisicing spare ribs proident ham hock, lorem fatback enim landjaeger.
+        Labore short ribs bresaola biltong, dolore frankfurter nostrud
+        exercitation sausage proident reprehenderit pork chop. Velit short ribs
+        bresaola esse in.
+      </p>
+
+      <p>
+        Ad id labore deserunt pastrami buffalo consectetur eiusmod biltong
+        pariatur bresaola kevin picanha burgdoggen. Jowl tri-tip fugiat, non in
+        eiusmod short ribs filet mignon dolore. Meatloaf esse nostrud ground
+        round frankfurter incididunt landjaeger corned beef nisi sirloin est
+        proident burgdoggen. Exercitation doner ea qui commodo ad labore tempor
+        sausage.
+      </p>
+
+      <p>
+        Burgdoggen chicken cillum shoulder consequat pork belly venison tail
+        beef. Bresaola capicola ground round, alcatra filet mignon excepteur in
+        ribeye. Enim biltong ipsum chislic, pancetta burgdoggen sunt anim
+        incididunt swine kevin cupim. Est qui aliquip leberkas meatball boudin
+        flank. Pastrami alcatra short ribs ea meatball. Porchetta occaecat cow
+        meatball exercitation, eiusmod turkey incididunt quis shoulder corned
+        beef filet mignon dolore.
+      </p>
+    </>
+  );
+};
+
+export default About;
