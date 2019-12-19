e34a3d087be3c15197ffabc09d43c6fe9b6eb1e6
bamfranceschi <anna.franceschi@gmail.com>
2019-12-19 16:36:07 -0600

    created header component

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <h2>Anna Franceschi</h2>
+        <h4>Web Developer</h4>
+        <h4>Austin, Tx</h4>
+      </div>
+      <div>
+        <img src="https://images.unsplash.com/photo-1560182704-284aa5d1b787?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"></img>
+      </div>
+    </div>
+  );
+};
